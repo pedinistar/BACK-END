ce3785590343a242161a8ee2a057862c3922e6cc
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-03-20 04:31:28 +0000

    .

--- dynamic_routing.js
+++ dynamic_routing.js
@@ -11,4 +11,13 @@ app.get("/profile", function (req, res) {
   res.send("This is Profile Route!");
 });
 
+app.get("/profile/:username", function (req, res) {
+  res.send(`This is ${req.params.username} Profile Route!`);
+});
+
+// url mein jab bhi aapke pass ek aisa pattern ho
+// /profile/:username (jidr colon ho)
+// :username -> params kehte hain isse
+// username ko access karne ke liye req mein params mein jakr username ko access kar skte hain
+
 app.listen(3000);
